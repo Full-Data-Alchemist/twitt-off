e13b1ed87c47590a9f8e9f4cbac1c708f3f5bd23
Full Data Alchemist <58147026+Full-Data-Alchemist@users.noreply.github.com>
2020-10-12 20:04:50 -0500

    create users

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,15 +2,30 @@
 Main app/routing file for twitoff
 """
 
-from flask import Flask
+from flask import Flask, render_template
+from .models import DB, User
 
 def  create_app():
-    app = Flask(__name__)
+    """
+    Create a config of the flask application
+    """
 
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3" #where DB is stores 
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+    DB.init__app(app)
     #TODO - make rest of the app
     @app.route('/')
     def root():
-        return render_template("base.html", title="Home")
+        DB.drop_all() #delates data base that is present
+        DB.create_all() #create the database from scratch
+        insert_example_users()
+        return render_template("base.html", title="Home", users=User.query.all())
+
+
+
+    #new run new application
+    #
     return app
 
     
--- twitoff/models.py
+++ twitoff/models.py
@@ -2,9 +2,7 @@
 # SQLAlchemy models and utility functions for twitoff
 """
 
-
 from flask_sqlalchemy import SQLAlchemy
-
 
 
 DB = SQLAlchemy()
@@ -12,10 +10,44 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     """
-    Twitter Users
+    Twitter Users corresponding tweets
     """
     id = DB.Column(DB.BigInterger, primary_key=True)
-    name = DB.COlumn(DB.String, nullable=False)
+    name = DB.Column(DB.String, nullable=False)
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
+
+
+class Tweet(DB.Model):
+    """
+    Tweets of the users
+    """
+    id = DB.Column(DB.BigInterger,
+                         DB.ForeignKey('user.id'), nullable=False)
+    text =DB.Column(DB.Unicode(300))
+
+    user_id = DB.Column(DB.BigInterger, 
+                                DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', 
+                                backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return "tweet:{}".format(self.text)
+
+
+    def  insert_example_users():
+        """
+        Example users
+        """
+        Mani = User(id=1, name="Mani")
+        Not_little = User(id=2, name="Edward Elric")
+        Body_less = User(id=3, name="Alphonse Elric")
+        Fallen_ishvalan = User(id=4, name="Scar")
+        
+        users_list = [Mani, Not_little, Body_less, Fallen_ishvalan]
+
+        for yin in users_list:
+            DB.session.add(yin)
+        
+        DB.session.commit()
